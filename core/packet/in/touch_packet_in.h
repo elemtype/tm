724d2faa21d9d79ae2864df5a331b177d92ee0b5
elemtype <elemtype@gmail.com>
2013-10-07 23:40:53 +0800

    a

---
+++ core/packet/in/touch_packet_in.h
@@ -0,0 +1,13 @@
+#ifndef TOUCH_PACKET_IN_H
+#define TOUCH_PACKET_IN_H
+
+#include "base_packet_in.h"
+
+class TouchPacketIn : public PacketIn{
+
+ public:
+ TouchPacketIn(byte*):PacketIn(byte*);
+  ~TouchPacketIn();
+};
+
+#endif
