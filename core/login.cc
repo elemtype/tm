af4d5e5ddd1f15278f9347d5b454442eecfa6566
elemtype <elemtype@gmail.com>
2013-10-08 20:10:39 +0800

    1

--- core/login.cc
+++ core/login.cc
@@ -3,6 +3,43 @@
 
 int main(int argc,char **argv)
 {
+  try
+    {
+
+      //ClientSocket client_socket ( "183.60.48.174", 8000 );
+      Socket *socket = new Socket();
+      socket->create();
+      socket->bind(0);
+      socket->connect("183.60.48.174", 8000);
+
+      byte sequence[] = {0x00,0x01};
+
+      TouchPacketIn *in = new TouchPacketIn(sequence);
+      
+      int reply_len = 0;
+      unsigned char *reply;
+      unsigned char *key = in->get_data();
+
+      reinterpret_cast<char*>(key);
+      //std::string ss(key);
+      try
+	{
+	  //socket << key;//"Test message.";
+	  //socket >> reply;
+          socket->send(key,115);
+          reply_len = socket->recv(reply);
+	}
+      catch ( SocketException& ) {}
+
+      pnt_byte(reply,reply_len);
+
+      std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+
+    }
+  catch ( SocketException& e )
+    {
+      std::cout << "Exception was caught:" << e.description() << "\n";
+    }
 
   return 0;
 }
--- core/login.h
+++ core/login.h
@@ -1,5 +1,13 @@
 #ifndef LOGON_H
 #define LOGON_H
 
+#include "../network/ClientSocket.h"
+#include "../network/SocketException.h"
+
+#include "../common/util.h"
+#include "../common/common.h"
+
+#include "packet/in/touch_packet_in.h"
+
 
 #endif
