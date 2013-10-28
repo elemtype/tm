ac52633fdc3ccfa63260d7ca3b36ec8c56cfacb2
elemtype <elemtype@gmail.com>
2013-10-28 21:20:26 +0800

    2013-10-28

--- common/global.h
+++ common/global.h
@@ -9,6 +9,8 @@ extern byte *g_0825_data;
 extern byte *g_0825_token;
 extern byte *g_0826_token;
 extern byte *g_0826_key;
+extern byte *g_0828_key_0;
+extern byte *g_0828_key_1;
 extern byte *g_local_ip;
 extern int  g_sequence;
 
